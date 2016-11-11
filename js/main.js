ab33c88bc74af6249764fc8b1f48ba440d6c8279
rensramos <rensramos17@gmail.com>
2016-11-11 12:16:17 +0800

    Add navbar hamburger

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
+//Navbar burger transition
+$('#icon-transition').on('click', function () {
+    $(this).toggleClass('open');
+});
+
 //On navbar scroll
 $(function () {
-    if (window.pageYOffset > 50 && $(document).width() >= 992) {
+    if (window.pageYOffset > 50 && $(document).width() >= 1281) {
         $('nav').addClass('navbar-shrink')
         $(window).scroll(function () {
-            if ($(document).scrollTop() > 50 && $(document).width() >= 992) {
+            if ($(document).scrollTop() > 50 && $(document).width() >= 1281) {
                 $('nav').addClass('navbar-shrink');
             } else {
                 $('nav').removeClass('navbar-shrink');
             }
         });
     }
-    else if (window.pageYOffset < 50 && $(document).width() >= 992) {
+    else if (window.pageYOffset < 50 && $(document).width() >= 1281) {
         $(window).scroll(function () {
-            if ($(document).scrollTop() > 50 && $(document).width() >= 992) {
+            if ($(document).scrollTop() > 50 && $(document).width() >= 1281) {
                 $('nav').addClass('navbar-shrink');
             } else {
                 $('nav').removeClass('navbar-shrink');
             }
         });
     }
-    if ($(document).width() <= 991 && window.pageYOffset > 50 || $(document).width() <= 991 && window.pageYOffset < 50) {
+    if ($(document).width() <= 991 && window.pageYOffset > 50 || $(document).width() <= 1281 && window.pageYOffset < 50) {
         $('nav').addClass('navbar-shrink');
     }
 });
