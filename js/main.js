7c17fb51d841ae305657595cb6bacf17cdcf7cda
rensramos <rensramos17@gmail.com>
2017-01-09 07:44:59 +0800

    Update js

--- js/main.js
+++ js/main.js
@@ -5,26 +5,26 @@ $('#icon-transition').on('click', function () {
 
 //On navbar scroll
 $(function () {
-    if (window.pageYOffset > 50 && $(document).width() >= 1281) {
+    if (window.pageYOffset > 50 && $(document).width() >= 1024) {
         $('nav').addClass('navbar-shrink')
         $(window).scroll(function () {
-            if ($(document).scrollTop() > 50 && $(document).width() >= 1281) {
+            if ($(document).scrollTop() > 50 && $(document).width() >= 1024) {
                 $('nav').addClass('navbar-shrink');
             } else {
                 $('nav').removeClass('navbar-shrink');
             }
         });
     }
-    else if (window.pageYOffset < 50 && $(document).width() >= 1281) {
+    else if (window.pageYOffset < 50 && $(document).width() >= 1024) {
         $(window).scroll(function () {
-            if ($(document).scrollTop() > 50 && $(document).width() >= 1281) {
+            if ($(document).scrollTop() > 50 && $(document).width() >= 1024) {
                 $('nav').addClass('navbar-shrink');
             } else {
                 $('nav').removeClass('navbar-shrink');
             }
         });
     }
-    if ($(document).width() <= 991 && window.pageYOffset > 50 || $(document).width() <= 1281 && window.pageYOffset < 50) {
+    if ($(document).width() <= 991 && window.pageYOffset > 50 || $(document).width() <= 1024 && window.pageYOffset < 50) {
         $('nav').addClass('navbar-shrink');
     }
 });
