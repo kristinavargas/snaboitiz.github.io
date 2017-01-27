040abc8cb42dfef97aca5428e13b76756009027d
rensramos <rensramos17@gmail.com>
2017-01-27 14:47:13 +0800

    Fix scrolling bug

--- js/home.js
+++ js/home.js
@@ -4,56 +4,53 @@ $('#fullpage').fullpage({
     hybrid:true,
     lockAnchors:true,
     navigation: false,
-    menu: '#menu',
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'Our Plants', 'News', 'SNAP-RES'],
-    showActiveTooltip: true,
-    controlArrows: false,
+    navigationTooltips: ['Home', 'Our Plants', 'News'],
     verticalCentered:false,
-    slidesNavigation:true,
     fitToSection:false,
     recordHistory:false,
-    anchors: ['section-1', 'section-2', 'section-3', 'section-4'],
+    keyboardScrolling: true,
+    anchors: ['section-1', 'section-2', 'section-3']
 
-    afterRender: function(){
-        var pluginContainer = $(this);
-        console.log("The resulting DOM structure is ready");
-    },
-
-    onLeave: function (index, nextIndex, direction) {
-        if (index == 2 && !sliding) {
-            if (direction == 'down' && slideIndex2 < 3) {
-                sliding = true;
-                $.fn.fullpage.moveSlideRight();
-                return false;
-            } else if (direction == 'up' && slideIndex2 > 1) {
-                sliding = true;
-                $.fn.fullpage.moveSlideLeft();
-                return false;
-            }
-        } else if (sliding) {
-            return false;
-        }
-
-    },
-
-    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-        sliding = false;
-    },
-
-    onSlideLeave  : function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
-        if (index == 2) {
-            if (direction == 'right') {
-                sliding = true;
-                slideIndex2++;
-            }
-
-            if (direction == 'left') {
-                sliding = true;
-                slideIndex2--;
-            }
-        }
-    }
+    // afterRender: function(){
+    //     var pluginContainer = $(this);
+    //     console.log("The resulting DOM structure is ready");
+    // },
+    //
+    // onLeave: function (index, nextIndex, direction) {
+    //     if (index == 2 && !sliding) {
+    //         if (direction == 'down' && slideIndex2 < 3) {
+    //             sliding = true;
+    //             $.fn.fullpage.moveSlideRight();
+    //             return false;
+    //         } else if (direction == 'up' && slideIndex2 > 1) {
+    //             sliding = true;
+    //             $.fn.fullpage.moveSlideLeft();
+    //             return false;
+    //         }
+    //     } else if (sliding) {
+    //         return false;
+    //     }
+    //
+    // },
+    //
+    // afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+    //     sliding = false;
+    // },
+    //
+    // onSlideLeave  : function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
+    //     if (index == 2) {
+    //         if (direction == 'right') {
+    //             sliding = true;
+    //             slideIndex2++;
+    //         }
+    //
+    //         if (direction == 'left') {
+    //             sliding = true;
+    //             slideIndex2--;
+    //         }
+    //     }
+    // }
 });
 
 
